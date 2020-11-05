58ce6cdf730670e9099bc69f6a422c9795e53d43
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-11-06 01:58:24 +0530

    [Prathamesh Rajput] Add. Completed Use Case 1

---
+++ Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    class Management
+    {
+        public DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> reviews)
+        {
+            var recordedData = (from productReview in reviews
+                                orderby productReview.Rating descending
+                                select productReview).Take(3);
+
+            foreach (var record in recordedData)
+                Console.WriteLine("ProductID: "+record.ProductId + " " + " UserId: " + record.UserId + " " + " Rating: " + record.Rating + " " + " Review: " + record.Review+ " Liked: " +record.isLike);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data;
+
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product review management problem statement");
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview(){ProductId=1,UserId=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductId=2,UserId=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductId=3,UserId=2,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductId=4,UserId=2,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductId=5,UserId=3,Rating=2,Review="nice",isLike=false},
+                new ProductReview(){ProductId=6,UserId=4,Rating=1,Review="Bad",isLike=false},
+                new ProductReview(){ProductId=1,UserId=3,Rating=1.5,Review="Good",isLike=false},
+                new ProductReview(){ProductId=11,UserId=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductId=10,UserId=10,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductId=12,UserId=10,Rating=7,Review="nice",isLike=true},
+                new ProductReview(){ProductId=13,UserId=10,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProductId=14,UserId=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductId=15,UserId=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ProductId=16,UserId=10,Rating=4,Review="nice",isLike=true},
+            };
+
+            Management management = new Management();
+            management.TopRecords(productReviews);
+
+
+            Console.Read();
+        }
+    }
+}
